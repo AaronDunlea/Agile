07ac08e24bed83fa174a189af2485a0ae76b6afd
Aaron Dunlea <aaron.dunlea@mycit.ie>
2017-11-29 13:17:03 +0000

    created hello file

---
+++ hello.java
@@ -0,0 +1,16 @@
+
+public class MyMain {
+
+
+	public static void printHello() {
+		
+		System.out.println("Hello World");
+	
+	}
+
+	public static void main(String[] args) {
+		//1. We call to the function testMyQueue
+		printHello();
+	}
+
+}
