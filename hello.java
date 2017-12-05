7146bf166ef2ac2ce31b2eb9908a387b02cd074b
Aaron Dunlea <aaron.dunlea@mycit.ie>
2017-12-05 10:15:32 +0000

    Pre demo test
    added print statement for test

--- hello.java
+++ hello.java
@@ -5,12 +5,14 @@ public class MyMain {
 	public static void printHello() {
 		
 		System.out.println("Hello World");
-	
+				System.out.println("This is a test - no. 1");
+
 	}
 
 	public static void main(String[] args) {
-		//1. We call to the function testMyQueue
+	
 		printHello();
+		
 	}
 
 }
